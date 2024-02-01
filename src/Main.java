d608cd677b19b9d267675f64c673d81f32dc0272
original <git@github.com:denis-poroshin/variables.git>
2024-02-01 20:22:34 +0300

    Исправлено

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,38 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задание 1, 2");
+        System.out.println("Задание 1");
         int clientOS = 1;
-        int yearOfIssue = 2015;
-        int myYearOfIssue = 2014;
         if (clientOS == 1){
             System.out.println("Установите версию приложения для Android по ссылке.");
-            if (myYearOfIssue <= yearOfIssue){
-                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
-            }
+
         }else if (clientOS == 0){
             System.out.println("Установите версию приложения для iOS по ссылке");
-            if (myYearOfIssue <= yearOfIssue){
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
-            }
 
             }
+        System.out.println("Задание 2"); // Поначалу думал, что нужно объеденить задание 1 и 2 =)
+        int yearOfIssue = 2015;
+        int myYearOfIssue = 2014;
+        if (clientOS == 1 && myYearOfIssue <= yearOfIssue) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+
+        }else if (clientOS == 1 && myYearOfIssue > yearOfIssue) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+
+        }else if (clientOS == 0 && myYearOfIssue <= yearOfIssue) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+
+        }else if (clientOS == 0 && myYearOfIssue > yearOfIssue) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
         System.out.println("Задание 3");
-        int year = 2021;
-        if (year % 4 == 0 || year % 400 == 0){
-            System.out.println(year + " год является високосным");
 
-        }else if (year % 100 == 0){
-            System.out.println(year + " год не является високосным");
+        int year = 1700;
+        if ((year % 4 == 0 && year % 100 != 0 && year > 1584) || (year % 400 == 0 && year > 1584)){
+            System.out.println(year + " год является високосным");
 
         }else{
             System.out.println(year + " год не является високосным");
-
         }
         System.out.println("Задание 4");
         int deliveryDistance = 95;
@@ -45,44 +50,20 @@ public class Main {
             System.out.println("Доставки нет.");
 
         }
-        System.out.println("Задание 4");
-        int monthNumber = 5;
+        System.out.println("Задание 5"); // Не знал, что в кейсы можно записывать несколько значений =)
+        int monthNumber = 3;
         switch (monthNumber){
-            case 1:
+            case 1, 2, 12:
                 System.out.println("Зима");
                 break;
-            case 2:
-                System.out.println("Зима");
-                break;
-            case 3:
+            case 3, 4, 5:
                 System.out.println("Весна");
                 break;
-            case 4:
-                System.out.println("Весна");
-                break;
-            case 5:
-                System.out.println("Весна");
-                break;
-            case 6:
+            case 6, 7, 8:
                 System.out.println("Лето");
                 break;
-            case 7:
-                System.out.println("Лето");
-                break;
-            case 8:
-                System.out.println("Лето");
-                break;
-            case 9:
+            case 9, 10, 11:
                 System.out.println("Осень");
-                break;
-            case 10:
-                System.out.println("Осень");
-                break;
-            case 11:
-                System.out.println("Осень");
-                break;
-            case 12:
-                System.out.println("Зима");
                 break;
             default:
                 System.out.println("Нет такого времени года");
